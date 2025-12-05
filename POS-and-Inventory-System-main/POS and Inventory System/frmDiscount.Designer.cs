3a3141345babb83ca5775af2bf59b2bec98c7088
ACE <asaexcelelgib@gmail.com>
2025-12-05 13:31:35 +0700

    ui update

--- POS-and-Inventory-System-main/POS and Inventory System/frmDiscount.Designer.cs
+++ POS-and-Inventory-System-main/POS and Inventory System/frmDiscount.Designer.cs
@@ -36,11 +36,11 @@
             this.txtPercent = new System.Windows.Forms.TextBox();
             this.label4 = new System.Windows.Forms.Label();
             this.txtAmount = new System.Windows.Forms.TextBox();
-            this.btnSave = new System.Windows.Forms.Button();
             this.lblId = new System.Windows.Forms.Label();
             this.btnClose = new System.Windows.Forms.Button();
             this.txtPCode = new System.Windows.Forms.TextBox();
             this.label5 = new System.Windows.Forms.Label();
+            this.guna2ButtonConfirm = new Guna.UI2.WinForms.Guna2Button();
             this.SuspendLayout();
             // 
             // label1
@@ -52,7 +52,7 @@
             this.label1.ForeColor = System.Drawing.Color.White;
             this.label1.Location = new System.Drawing.Point(0, 0);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(434, 25);
+            this.label1.Size = new System.Drawing.Size(492, 25);
             this.label1.TabIndex = 0;
             this.label1.Text = "- D I S C O U N T -";
             this.label1.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
@@ -65,16 +65,16 @@
             this.label2.ForeColor = System.Drawing.Color.Black;
             this.label2.Location = new System.Drawing.Point(9, 85);
             this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(94, 16);
+            this.label2.Size = new System.Drawing.Size(112, 20);
             this.label2.TabIndex = 3;
             this.label2.Text = "TOTAL PRICE";
             // 
             // txtPrice
             // 
-            this.txtPrice.Location = new System.Drawing.Point(142, 81);
+            this.txtPrice.Location = new System.Drawing.Point(184, 82);
             this.txtPrice.Name = "txtPrice";
             this.txtPrice.ReadOnly = true;
-            this.txtPrice.Size = new System.Drawing.Size(280, 22);
+            this.txtPrice.Size = new System.Drawing.Size(280, 26);
             this.txtPrice.TabIndex = 10;
             // 
             // label3
@@ -85,15 +85,15 @@
             this.label3.ForeColor = System.Drawing.Color.Black;
             this.label3.Location = new System.Drawing.Point(7, 114);
             this.label3.Name = "label3";
-            this.label3.Size = new System.Drawing.Size(100, 16);
+            this.label3.Size = new System.Drawing.Size(120, 20);
             this.label3.TabIndex = 3;
             this.label3.Text = "DISCOUNT (%)";
             // 
             // txtPercent
             // 
-            this.txtPercent.Location = new System.Drawing.Point(142, 110);
+            this.txtPercent.Location = new System.Drawing.Point(184, 111);
             this.txtPercent.Name = "txtPercent";
-            this.txtPercent.Size = new System.Drawing.Size(280, 22);
+            this.txtPercent.Size = new System.Drawing.Size(280, 26);
             this.txtPercent.TabIndex = 0;
             this.txtPercent.TextChanged += new System.EventHandler(this.TxtDiscount_TextChanged);
             // 
@@ -105,32 +105,17 @@
             this.label4.ForeColor = System.Drawing.Color.Black;
             this.label4.Location = new System.Drawing.Point(7, 142);
             this.label4.Name = "label4";
-            this.label4.Size = new System.Drawing.Size(139, 16);
+            this.label4.Size = new System.Drawing.Size(164, 20);
             this.label4.TabIndex = 3;
             this.label4.Text = "DISCOUNT AMOUNT";
             // 
             // txtAmount
             // 
-            this.txtAmount.Location = new System.Drawing.Point(142, 139);
+            this.txtAmount.Location = new System.Drawing.Point(184, 140);
             this.txtAmount.Name = "txtAmount";
             this.txtAmount.ReadOnly = true;
-            this.txtAmount.Size = new System.Drawing.Size(280, 22);
+            this.txtAmount.Size = new System.Drawing.Size(280, 26);
             this.txtAmount.TabIndex = 10;
-            // 
-            // btnSave
-            // 
-            this.btnSave.BackColor = System.Drawing.Color.Black;
-            this.btnSave.FlatAppearance.BorderSize = 0;
-            this.btnSave.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
-            this.btnSave.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F);
-            this.btnSave.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(225)))), ((int)(((byte)(228)))), ((int)(((byte)(221)))));
-            this.btnSave.Location = new System.Drawing.Point(330, 168);
-            this.btnSave.Name = "btnSave";
-            this.btnSave.Size = new System.Drawing.Size(92, 29);
-            this.btnSave.TabIndex = 5;
-            this.btnSave.Text = "CONFIRM";
-            this.btnSave.UseVisualStyleBackColor = false;
-            this.btnSave.Click += new System.EventHandler(this.BtnSave_Click);
             // 
             // lblId
             // 
@@ -138,7 +123,7 @@
             this.lblId.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(19)))), ((int)(((byte)(36)))), ((int)(((byte)(71)))));
             this.lblId.Location = new System.Drawing.Point(12, 25);
             this.lblId.Name = "lblId";
-            this.lblId.Size = new System.Drawing.Size(0, 16);
+            this.lblId.Size = new System.Drawing.Size(0, 20);
             this.lblId.TabIndex = 6;
             // 
             // btnClose
@@ -150,7 +135,7 @@
             this.btnClose.FlatAppearance.MouseOverBackColor = System.Drawing.Color.FromArgb(((int)(((byte)(19)))), ((int)(((byte)(36)))), ((int)(((byte)(71)))));
             this.btnClose.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
             this.btnClose.Image = ((System.Drawing.Image)(resources.GetObject("btnClose.Image")));
-            this.btnClose.Location = new System.Drawing.Point(384, 0);
+            this.btnClose.Location = new System.Drawing.Point(442, 0);
             this.btnClose.Name = "btnClose";
             this.btnClose.Size = new System.Drawing.Size(50, 25);
             this.btnClose.TabIndex = 16;
@@ -159,10 +144,10 @@
             // 
             // txtPCode
             // 
-            this.txtPCode.Location = new System.Drawing.Point(142, 53);
+            this.txtPCode.Location = new System.Drawing.Point(184, 54);
             this.txtPCode.Name = "txtPCode";
             this.txtPCode.ReadOnly = true;
-            this.txtPCode.Size = new System.Drawing.Size(280, 22);
+            this.txtPCode.Size = new System.Drawing.Size(280, 26);
             this.txtPCode.TabIndex = 18;
             // 
             // label5
@@ -173,23 +158,40 @@
             this.label5.ForeColor = System.Drawing.Color.Black;
             this.label5.Location = new System.Drawing.Point(9, 57);
             this.label5.Name = "label5";
-            this.label5.Size = new System.Drawing.Size(105, 16);
+            this.label5.Size = new System.Drawing.Size(127, 20);
             this.label5.TabIndex = 17;
             this.label5.Text = "PRODUK CODE";
             // 
+            // guna2ButtonConfirm
+            // 
+            this.guna2ButtonConfirm.BorderRadius = 10;
+            this.guna2ButtonConfirm.DisabledState.BorderColor = System.Drawing.Color.DarkGray;
+            this.guna2ButtonConfirm.DisabledState.CustomBorderColor = System.Drawing.Color.DarkGray;
+            this.guna2ButtonConfirm.DisabledState.FillColor = System.Drawing.Color.FromArgb(((int)(((byte)(169)))), ((int)(((byte)(169)))), ((int)(((byte)(169)))));
+            this.guna2ButtonConfirm.DisabledState.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(141)))), ((int)(((byte)(141)))), ((int)(((byte)(141)))));
+            this.guna2ButtonConfirm.FillColor = System.Drawing.Color.Black;
+            this.guna2ButtonConfirm.Font = new System.Drawing.Font("Segoe UI", 9F);
+            this.guna2ButtonConfirm.ForeColor = System.Drawing.Color.White;
+            this.guna2ButtonConfirm.Location = new System.Drawing.Point(369, 172);
+            this.guna2ButtonConfirm.Name = "guna2ButtonConfirm";
+            this.guna2ButtonConfirm.Size = new System.Drawing.Size(95, 32);
+            this.guna2ButtonConfirm.TabIndex = 19;
+            this.guna2ButtonConfirm.Text = "Confirm";
+            this.guna2ButtonConfirm.Click += new System.EventHandler(this.guna2ButtonConfirm_Click);
+            // 
             // frmDiscount
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.Color.White;
-            this.ClientSize = new System.Drawing.Size(434, 213);
+            this.ClientSize = new System.Drawing.Size(492, 213);
             this.ControlBox = false;
+            this.Controls.Add(this.guna2ButtonConfirm);
             this.Controls.Add(this.txtPCode);
             this.Controls.Add(this.label5);
             this.Controls.Add(this.btnClose);
             this.Controls.Add(this.lblId);
             this.Controls.Add(this.label1);
-            this.Controls.Add(this.btnSave);
             this.Controls.Add(this.txtAmount);
             this.Controls.Add(this.txtPercent);
             this.Controls.Add(this.label4);
@@ -201,6 +203,7 @@
             this.Margin = new System.Windows.Forms.Padding(4);
             this.Name = "frmDiscount";
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Load += new System.EventHandler(this.frmDiscount_Load);
             this.KeyDown += new System.Windows.Forms.KeyEventHandler(this.FrmDiscount_KeyDown);
             this.ResumeLayout(false);
             this.PerformLayout();
@@ -214,11 +217,11 @@
         private System.Windows.Forms.TextBox txtPercent;
         private System.Windows.Forms.Label label4;
         private System.Windows.Forms.TextBox txtAmount;
-        private System.Windows.Forms.Button btnSave;
         public System.Windows.Forms.Label lblId;
         public System.Windows.Forms.TextBox txtPrice;
         private System.Windows.Forms.Button btnClose;
         public System.Windows.Forms.TextBox txtPCode;
         private System.Windows.Forms.Label label5;
+        private Guna.UI2.WinForms.Guna2Button guna2ButtonConfirm;
     }
 }
